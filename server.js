7fb7631143b89e7ab9af8510f180143a790dafeb
ehteshamtarq <ehteshamtarique1@gmail.com>
2024-04-02 16:55:26 +0530

    package.json

---
+++ server.js
@@ -0,0 +1,19 @@
+require('./config/db');
+require("dotenv").config();
+const app = require('express')();
+const port = 3000;
+const router = require("./api/User.js");
+const morgan = require("morgan");
+const cors = require('cors');
+
+
+
+const bodyParser = require('express').json;
+app.use(bodyParser());
+app.use(morgan('dev'))
+app.use(cors());
+app.use("/users", router);
+
+app.listen(port, ()=>{
+    console.log(`Server listening on ${port}`);
+})
